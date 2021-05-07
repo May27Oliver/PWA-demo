cbfe59327eeabb11c9c63be09d026f0f910cac61
May27Oliver <oliver310338@hotmail.com>
2021-05-07 13:37:01 +0800

    add service worker route for firebase

--- src/service-worker.js
+++ src/service-worker.js
@@ -85,3 +85,8 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+registerRoute('wss://s-apse1b-nss-209.asia-southeast1.firebasedatabase.app/.ws?v=5&ns=todolistpractice-df8c0-default-rtdb',
+  new StaleWhileRevalidate({
+    cacheName: 'firebase'
+  })
+)
